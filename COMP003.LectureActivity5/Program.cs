6a8836ec346e3023230951acd4c18ddfb6e699f6
samian <sam.iannazzo@yahoo.com>
2026-04-18 14:40:04 -0700

    added swagger to service container in program.cs

--- COMP003.LectureActivity5/Program.cs
+++ COMP003.LectureActivity5/Program.cs
@@ -8,8 +8,10 @@ namespace COMP003.LectureActivity5
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            // added swagger to the service containers
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -18,7 +20,8 @@ namespace COMP003.LectureActivity5
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
