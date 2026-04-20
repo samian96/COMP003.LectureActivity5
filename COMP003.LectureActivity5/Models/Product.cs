0d733b3d10b0306310f0e0b7283ef6e8cd610aae
samian <sam.iannazzo@yahoo.com>
2026-04-19 22:41:27 -0700

    created folder for models / added product.cs

---
+++ COMP003.LectureActivity5/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP003.LectureActivity5.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
--- COMP003.LectureActivity5/Program.cs
+++ COMP003.LectureActivity5/Program.cs
@@ -7,12 +7,16 @@ namespace COMP003.LectureActivity5
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -20,7 +24,10 @@ namespace COMP003.LectureActivity5
             }
 
             app.UseHttpsRedirection();
+
             app.UseAuthorization();
+
+
             app.MapControllers();
 
             app.Run();
