81e3ca09e8ce12cd12da4e42d31406376816ced8
samian <sam.iannazzo@yahoo.com>
2026-04-19 23:01:25 -0700

    added ApiController, route, http get for list and id

---
+++ COMP003.LectureActivity5/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using COMP003.LectureActivity5.Data;
+using COMP003.LectureActivity5.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003.LectureActivity5.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return Ok(ProductStore.Products);
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = ProductStore.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
