7f67c66e5ad744bdce080789fc3057aa718d418a
samian <sam.iannazzo@yahoo.com>
2026-04-19 22:45:29 -0700

    added Data folder / new products list

---
+++ COMP003.LectureActivity5/Data/ProductStore.cs
@@ -0,0 +1,14 @@
+﻿using COMP003.LectureActivity5.Models;  
+
+namespace COMP003.LectureActivity5.Data
+{
+    public class ProductStore
+    {
+        public static List<Product> Products { get; } = new()
+        {
+            new Product {Id =1, Name = "Laptop", Price = 1200.00m },
+            new Product {Id = 2, Name = "Mouse", Price = 25.99m },
+            new Product {Id = 3, Name = "Keyboard", Price = 49.99m }
+        };
+    }
+}
